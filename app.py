975fee3ae1d545fdc2f3b6be101f88c8057b39f7
Christian Machado <realchris.machado@gmail.com>
2024-09-30 20:25:36 -0300

    Adicionando um novo pacote 'purchase_order_items' e junto com ele um método GIT para listagem dos itens de cada pedido de compra

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_restful import Api
 
 from porders.resources import PurchaseOrders, PurchaseOrdersById
+from purchase_orders_items.resources import PurchaseOrdersItems
 
 app = Flask(__name__) # Armazena nessa variavel o nome do arquivo
 api = Api(app)
     
 api.add_resource(PurchaseOrders, '/purchase_orders')
 api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
+api.add_resource(PurchaseOrdersItems, '/purchase_orders/<int:id>/items')
 
 app.run(port=5000) # Executar nossa aplicação na porta 5000
---
+++ purchase_orders_items/resources.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+from flask_restful import Resource
+
+
+purchase_orders = [
+    {
+    'id': 1,
+    'description': 'Pedido de de Compra 1',
+    'items': [
+        {
+            'id': 1,
+            'description': 'Item do pedido 1',
+            'price': 20.99
+        }
+    ]
+    }
+]
+
+class PurchaseOrdersItems(Resource):
+    def get(self, id):
+        for po in purchase_orders:
+            if po['id'] == id:
+                return jsonify(po['items'])
+        return jsonify({'message': f'Pedido {id} não encontrado.'})
